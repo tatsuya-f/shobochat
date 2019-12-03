4676ea485c0d15ec6b6be5e7611107596f0a034d
ee177144 <ee177144.meiji.ac.jp>
2019-12-03 18:42:47 +0900

    fix: dom operation with tagname to id

--- src/public/js/client.ts
+++ src/public/js/client.ts
@@ -1,31 +1,31 @@
 "use strict";
 
 $(() => {
-    var $update_button = $("#update");
-    var $sendButton = $("#send");
+    const $updateButton = $("#update");
+    const $sendButton = $("#send");
 
     fetch("/messages")
         .then((response) => {
             return response.json();
         })
         .then((messages) => {
-            var $message_list = $("div");
-            $message_list.empty();
+            const $messageList = $("#messageList");
+            $messageList.empty();
             messages.forEach((message) => {
-                $message_list.append(`<p>time:${message.time} name:${message.name} message:${message.message}</p>`);
+                $messageList.append(`<p>time:${message.time} name:${message.name} message:${message.message}</p>`);
             });
         })
         .catch((err) => {
             console.log(err);
         });
 
-    $update_button.on("click", () => {
+    $updateButton.on("click", () => {
         fetch("/messages")
             .then((response) => {
                 return response.json();
             })
             .then((messages) => {
-                var $messageList = $("div");
+                const $messageList = $("#messageList");
                 $messageList.empty();
                 messages.forEach((message) => {
                     $messageList.append(`<p>time:${message.time} name:${message.name} message:${message.message}</p>`);
@@ -37,8 +37,8 @@ $(() => {
     });
 
     $sendButton.on("click", () => {
-        var requestBody = {
-            name: $("#name").val(), 
+        const requestBody = {
+            name: $("#name").val(),
             message: $("#message").val()
         };
         fetch("/messages", {
