404f2115d3c5ff19e57b720f1e3c6dd48757e904
ee177144 <ee177144@meiji.ac.jp>
2019-12-09 11:09:35 +0900

    fix test

--- test/hasChar.spec.ts
+++ test/hasChar.spec.ts
@@ -1,33 +1,33 @@
 import { equal } from "assert";
-import { hasSpace } from "../src/public/js/messageHandler";
-describe("hasSpace", function() {
-    describe("#hasSpace(a: string)", function() {
+import { hasChar } from "../src/public/js/messageHandler";
+describe("hasChar", function() {
+    describe("#hasChar(a: string)", function() {
         it("should return true when input is ABC", function() {
-            equal(hasSpace("ABC"), true);
+            equal(hasChar("ABC"), true);
         });
         it("should return true when input is DEFG", function() {
-            equal(hasSpace("DEFG"), true);
+            equal(hasChar("DEFG"), true);
         });
         it("should return false when input is empty", function() {
-            equal(hasSpace(""), false);
+            equal(hasChar(""), false);
         });
         it("should return false when input is '   '(3 spaces)", function() {
-            equal(hasSpace("   "), false);
+            equal(hasChar("   "), false);
         });
         it("should return false when input is '		'(two tabs)", function() {
-            equal(hasSpace("		"), false);
+            equal(hasChar("		"), false);
         });
         it("should return false when input is '   		'(3 spaces and two tabs)", function() {
-            equal(hasSpace("   		"), false);
+            equal(hasChar("   		"), false);
         });
         it("should return true when input is 'a   '(3 spaces)", function() {
-            equal(hasSpace("a   "), true);
+            equal(hasChar("a   "), true);
         });
         it("should return true when input is '	b	'(two tabs)", function() {
-            equal(hasSpace("	b	"), true);
+            equal(hasChar("	b	"), true);
         });
         it("should return true when input is '   		c'(3 spaces and two tabs)", function() {
-            equal(hasSpace("   		c"), true);
+            equal(hasChar("   		c"), true);
         });
     });
 });
