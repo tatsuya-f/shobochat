e13294298fea138f369cc32a3fd88cd2b233f6a8
tsubota-kouga <kouga.infini@gmail.com>
2019-12-10 21:51:56 +0900

    reflect new Message.ts

--- src/Message.ts
+++ src/Message.ts
@@ -9,8 +9,8 @@ export interface Message {
 
 export function isMessage(arg: any): arg is Message {
     return (arg.id === undefined || typeof arg.id === "number") &&
-        (arg.userId === undefined || typeof arg.id === "number") &&
-        (arg.time === undefined || typeof arg.id === "number") &&
+        (arg.userId === undefined || typeof arg.userId === "number") &&
+        (arg.time === undefined || typeof arg.time === "number") &&
         typeof arg.name === "string" &&
         typeof arg.message === "string";
 }
