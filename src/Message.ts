86cfb3d866d846b63c5e6c01c9a478da0e3b8020
tsubota-kouga <kouga.infini@gmail.com>
2019-12-10 21:26:27 +0900

    change to symbolic

--- src/Message.ts
+++ src/Message.ts
@@ -1,11 +1,16 @@
 
 export interface Message {
     id?: number;
+    userId?: number;
     time?: number;
     name: string;
     message: string;
 }
 
 export function isMessage(arg: any): arg is Message {
-    return typeof arg.name === "string" && typeof arg.message === "string" && (arg.id === undefined || typeof arg.id === "number") && (arg.id === undefined || typeof arg.id === "number");
+    return (arg.id === undefined || typeof arg.id === "number") &&
+        (arg.userId === undefined || typeof arg.id === "number") &&
+        (arg.time === undefined || typeof arg.id === "number") &&
+        typeof arg.name === "string" &&
+        typeof arg.message === "string";
 }
