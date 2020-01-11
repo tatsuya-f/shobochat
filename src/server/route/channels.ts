a92cd20a69a400532b14aa552a5aec8d39bca05a
ee177144 <ee177144@meiji.ac.jp>
2020-01-11 20:53:12 +0900

    fix db usage

--- src/server/route/channels.ts
+++ src/server/route/channels.ts
@@ -1,23 +1,20 @@
 import * as express from "express";
 import { Request, Response, NextFunction } from "express";
-import { getConnection } from "typeorm";
+import { DatabaseManager } from "../database/DatabaseManager";
 import { ChannelRepository } from "../repository/ChannelRepository";
 import { notifyNewChannel } from "../handler/webSocketHandler";
 
 export const channelsRouter = express.Router();
-const connectionType: string = process.env.TYPEORM_CONNECTION_TYPE || "default";
 
 channelsRouter.post("/:channel", async (req: Request, res: Response, next: NextFunction) => {
-    const channelRepository = getConnection(connectionType)
-        .getCustomRepository(ChannelRepository); // global で宣言するとうまくいかない
+    const databaseManager = await DatabaseManager.getInstance();
+    const channelRepository = databaseManager.getRepository(ChannelRepository);
     try {
         const channel = req.params.channel;
-        console.log(channel);
         if (await channelRepository.hasName(channel)) {
             res.status(405).end();
         } else {
             const id = await channelRepository.insertAndGetId(channel);
-            console.log(id);
             notifyNewChannel(await channelRepository.getAll());
             res.status(200).end();
         }
